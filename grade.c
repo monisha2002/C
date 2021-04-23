54a1728a583838201b1ea8302f33387e0d143595
monisha2002 <77716940+monisha2002@users.noreply.github.com>
2021-04-23 22:44:33 +0530

    Add files via upload

---
+++ grade.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("Enter marks");
+	scanf("%d",&n);
+	if(n>=85)
+	{
+		printf("Grade A");
+	}
+	else if(n<85&&n>=70)
+	{
+		printf("Grade B");
+	}
+	else if(n<70&&n>=55)
+	{
+		printf("Grade C");	
+	}
+	else if(n<55&&n>=40)
+	{
+		printf("Grade D");	
+	}
+	else if(n<40)
+	{
+		printf("Grade F");
+	}
+	return 0;
+}
